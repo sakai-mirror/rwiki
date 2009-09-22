5d9be1e70ba8f7a1456d88a8c56f23c2897d523c
gsilver@umich.edu <gsilver@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-09-22 17:06:42 +0000

    SAK-16866  - invert recently  visited pages list order  - avoid scrollbars in tool when user created content includes an list of any type.
    git-svn-id: https://source.sakaiproject.org/svn/rwiki/branches/SAK-16866@66726 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- rwiki-tool/tool/src/webapp/scripts/utils.js
+++ rwiki-tool/tool/src/webapp/scripts/utils.js
@@ -1,7 +1,6 @@
-
 /**********************************************************************************
  * $URL: https://source.sakaiproject.org/svn/rwiki/trunk/rwiki-tool/tool/src/webapp/scripts/ajaxpopup.js $
- * $Id: ajaxpopup.js 51318 2008-08-24 05:28:47Z csev@umich.edu $
+ * $Id: utils.js 51318 2008-08-24 05:28:47Z csev@umich.edu $
  ***********************************************************************************
  *
  * Copyright (c) 2003, 2004, 2005, 2006 Sakai Foundation
@@ -51,25 +50,32 @@ utils.resizeFrame = function(updown){
 
 
 rearrangeBreadCrumb = function(){
+	//start off hidden
     $('#wikiCrumb').hide();
-    $('#wikiCrumb li:last-child').remove();
+	//if any children, show menu and list
     if ($('#wikiCrumb li').length > 0) {
         $('#visitedPages').show();
     }
+	//if large, add class with overflow
     if ($("#wikiCrumb").height() > 200) {
-        $("#wikiCrumb").addClass("oversizeCrumb")
+        $("#wikiCrumb").addClass("oversizeCrumb");
     }
-    
+	//invert list so more recent go at top
+	$("#wikiCrumb li").each(function(){
+		$("#wikiCrumb").prepend(this);
+	});
+	//get position of menu open link
     var pos = $('#visitedPages').position();
     $('#visitedPages').click(function(event){
+		//set position of menu a bit below menu open link and show
         $('#wikiCrumb').css('top', pos.top + $('#visitedPages').height() + 5);
         $('#wikiCrumb').toggle();
         $('#visitedPages').toggleClass('visitedPagesOff');
         event.preventDefault();
     });
-}
+};
 
-
+// IE8 has a bug with textareas whose widths are set in css  - see SAK-16714
 utils.fixIE8TextArea = function(){
     var initIETaWidth;
     var initIETaCols;
@@ -80,7 +86,7 @@ utils.fixIE8TextArea = function(){
     if ($(ta).length) {
         //get init width and cols values
         initIETaWidth = $(ta).width();
-        initIETaCols = $(ta).attr('cols')
+        initIETaCols = $(ta).attr('cols');
         
         //set the width to auto to fix IE 8 textarea bugs
         $(ta).css('width', 'auto');
@@ -88,16 +94,17 @@ utils.fixIE8TextArea = function(){
         //check the new width
         autoIETaWidth = $(ta).width();
         
-        theseCols = initIETaCols
+        theseCols = initIETaCols;
         /*
          is it smaller than the init? Add a column till it is about the same
          size as the initial size
          */
         while (autoIETaWidth < (initIETaWidth - 20)) {
-            $(ta).attr('cols', theseCols + 1)
+            $(ta).attr('cols', theseCols + 1);
             theseCols = $(ta).attr('cols');
             autoIETaWidth = $(ta).width();
         }
         
     }
-}
+};
+
